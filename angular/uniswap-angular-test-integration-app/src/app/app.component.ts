f3ad638e01eed46255c2538d09edd8684a220286
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-04-28 16:02:40 +0100

    handle settings UI html only

--- angular/uniswap-angular-test-integration-app/src/app/app.component.ts
+++ angular/uniswap-angular-test-integration-app/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'uniswap-angular-test-integration-app';
+
+  public inputCurrency = '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b';
+  public outputCurrency = '0x1985365e9f78359a9B6AD760e32412f4a445E862';
+  public supportedContracts: string[] = [
+    '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b',
+    '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+  ];
+
+  constructor() {
+    // (<any>window).ethereum.request({ method: 'eth_requestAccounts' });
+  }
 }
