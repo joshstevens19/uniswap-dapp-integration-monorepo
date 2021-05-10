0d056f8ac3511ce2eb57cffccdde7e7413dfed06
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-05-10 16:47:16 +0100

    abstract some of the methods in the app.component.ts

--- angular/uniswap-angular-test-integration-app/src/app/app.component.ts
+++ angular/uniswap-angular-test-integration-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import BigNumber from 'bignumber.js';
 import { UniswapDappSharedLogic } from './uniswap-dapp-shared-logic';
 
 @Component({
@@ -11,12 +12,16 @@ export class AppComponent implements OnInit {
 
   public uniswapDappSharedLogic = new UniswapDappSharedLogic({
     inputCurrency: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b';
+    outputCurrency: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
     supportedContracts: [
-      '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b',
-      '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+      { contractAddress: '0x419D0d8BdD9aF5e606Ae2232ed285Aff190E711b' },
+      { contractAddress: '0x1985365e9f78359a9B6AD760e32412f4a445E862' },
     ],
   });
   public loading = true;
+
+  public inputValue = '1';
+  public outputValue = '0';
 
   constructor() {
     // (<any>window).ethereum.request({ method: 'eth_requestAccounts' });
@@ -25,10 +30,37 @@ export class AppComponent implements OnInit {
   public async ngOnInit(): Promise<void> {
     await this.uniswapDappSharedLogic.init();
 
+    if (this.uniswapDappSharedLogic.tradeContext?.expectedConvertQuote) {
+      this.outputValue = this.uniswapDappSharedLogic.tradeContext.expectedConvertQuote;
+    }
+
     this.loading = false;
   }
 
   public toggleSettings(): void {
     this.uniswapDappSharedLogic.toggleSettings();
+  }
+
+  public openTokenSelectorFrom(): void {
+    this.uniswapDappSharedLogic.openTokenSelectorFrom();
+  }
+
+  public openTokenSelectorTo(): void {
+    this.uniswapDappSharedLogic.openTokenSelectorTo();
+  }
+
+  public hideTokenSelector(): void {
+    this.uniswapDappSharedLogic.hideTokenSelector();
+  }
+
+  public async changeInputTradePrice(amount: string): Promise<void> {
+    this.inputValue = amount;
+    if (new BigNumber(this.inputValue).isEqualTo(0)) {
+      this.outputValue = '0';
+      return;
+    }
+
+    await this.uniswapDappSharedLogic.changeInputTradePrice(amount);
+    this.outputValue = this.uniswapDappSharedLogic.tradeContext!.expectedConvertQuote;
   }
 }
