0407f52f1862d7915e5189b77d338aaafa42d801
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-05-10 16:46:37 +0100

    shared dapp logic

--- angular/uniswap-angular-test-integration-app/src/app/uniswap-dapp-shared-logic.ts
+++ angular/uniswap-angular-test-integration-app/src/app/uniswap-dapp-shared-logic.ts
@@ -1,22 +1,39 @@
 /// TODO MOVE TO NPM PACKAGE ONCE HAPPY
 
 import BigNumber from 'bignumber.js';
-import { ChainId, Token, TokenFactoryPublic } from 'simple-uniswap-sdk';
+import {
+  ChainId,
+  Token,
+  TokenFactoryPublic,
+  TradeContext,
+  UniswapPair,
+  UniswapPairFactory,
+} from 'simple-uniswap-sdk';
 
 export interface UniswapDappSharedLogicContext {
   // ethereumAddress: string;
   inputCurrency?: string | undefined;
-  ouputCurrency?: string | undefined;
-  // look into a default token list??
-  supportedContracts: string[];
+  outputCurrency?: string | undefined;
+  supportedContracts: SupportedToken[];
 }
 
 export interface ExtendedToken extends Token {
   balance?: BigNumber | undefined;
 }
 
+export interface SupportedToken {
+  iconUrl?: string;
+  contractAddress: string;
+}
+
 export class UniswapDappSharedLogic {
   public inputToken!: ExtendedToken;
+  public outputToken: ExtendedToken | undefined;
+  public factory: UniswapPairFactory | undefined;
+  public tradeContext: TradeContext | undefined;
+  public slippage = 0.05;
+
+  private _inputAmount = new BigNumber(1);
 
   constructor(private _context: UniswapDappSharedLogicContext) {}
 
@@ -28,6 +45,14 @@ export class UniswapDappSharedLogic {
       this._context?.inputCurrency ||
         '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     );
+
+    if (this._context.outputCurrency) {
+      this.outputToken = await this.getTokenInformation(
+        this._context.outputCurrency
+      );
+
+      await this.buildFactory();
+    }
   }
 
   /**
@@ -42,6 +67,57 @@ export class UniswapDappSharedLogic {
     } else {
       settingsElement.classList.add('uni-ic-hidden');
     }
+  }
+
+  /**
+   * Open token selector from
+   */
+  public openTokenSelectorFrom(): void {
+    this.showTokenSelector();
+  }
+
+  /**
+   * Open token selector to
+   */
+  public openTokenSelectorTo(): void {
+    this.showTokenSelector();
+  }
+
+  /**
+   * Hide the token selector
+   */
+  public hideTokenSelector(): void {
+    const modal = document.getElementById('uni-ic__model')!;
+    modal.style.display = 'none';
+  }
+
+  /**
+   * Change input token
+   * @param contractAddress The contract address
+   */
+  public async changeInputToken(contractAddress: string): Promise<void> {
+    this.inputToken = await this.getTokenInformation(contractAddress);
+    await this.buildFactory();
+  }
+
+  /**
+   * Change output token
+   * @param contractAddress The contract address
+   */
+  public async changeOutputToken(contractAddress: string): Promise<void> {
+    this.outputToken = await this.getTokenInformation(contractAddress);
+    await this.buildFactory();
+  }
+
+  /**
+   * Change input trade price
+   */
+  public async changeInputTradePrice(amount: string): Promise<void> {
+    this._inputAmount = new BigNumber(amount);
+    if (!this.factory) {
+      await this.buildFactory();
+    }
+    await this.trade(this._inputAmount);
   }
 
   /**
@@ -82,5 +158,49 @@ export class UniswapDappSharedLogic {
     }
 
     return token;
+  }
+
+  /**
+   * Build factory
+   */
+  private async buildFactory(): Promise<void> {
+    const uniswapPair = new UniswapPair({
+      fromTokenContractAddress: this.inputToken.contractAddress,
+      toTokenContractAddress: this.outputToken!.contractAddress,
+      // the ethereum address of the user using this part of the dApp
+      ethereumAddress: '0xB1E6079212888f0bE0cf55874B2EB9d7a5e02cD9',
+      chainId: ChainId.MAINNET,
+    });
+
+    this.factory = await uniswapPair.createFactory();
+    await this.trade(this._inputAmount);
+  }
+
+  /**
+   * Show the token selector
+   */
+  private showTokenSelector(): void {
+    const modal = document.getElementById('uni-ic__model-token')!;
+    modal.style.display = 'block';
+  }
+
+  /**
+   * Execute the trade quote
+   * @param amount The amount
+   */
+  private async trade(amount: BigNumber): Promise<void> {
+    if (amount.isGreaterThan(0)) {
+      this.tradeContext = await this.factory!.trade(
+        this._inputAmount.toFixed()
+      );
+
+      console.log(this.tradeContext);
+
+      this.tradeContext?.quoteChanged$.subscribe((quote) => {
+        this.tradeContext = quote;
+      });
+    } else {
+      this.factory = undefined;
+    }
   }
 }
