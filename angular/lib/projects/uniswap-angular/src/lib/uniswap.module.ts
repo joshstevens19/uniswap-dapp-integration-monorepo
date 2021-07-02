d7446aa0cdcac8114c775f3ce98563146f914551
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-07-02 16:54:01 +0100

    more stuff for the angular lib

--- angular/lib/projects/uniswap-angular/src/lib/uniswap.module.ts
+++ angular/lib/projects/uniswap-angular/src/lib/uniswap.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { SafePipe } from './pipes/safe.pipe';
 import { UniswapAngularSwapperComponent } from './uniswap-angular-swapper/uniswap-angular-swapper.component';
 
 @NgModule({
   declarations: [SafePipe, UniswapAngularSwapperComponent],
-  imports: [CommonModule],
-  exports: [],
+  imports: [CommonModule, FormsModule, BrowserModule],
+  exports: [UniswapAngularSwapperComponent],
 })
 export class UniswapModule {}
--- angular/lib/projects/uniswap-angular/src/public-api.ts
+++ angular/lib/projects/uniswap-angular/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of uniswap
  */
 
+export * from './lib/uniswap-angular-swapper/uniswap-angular-swapper.component';
 export * from './lib/uniswap.module';
