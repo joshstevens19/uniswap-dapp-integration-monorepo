46597bd9a29ee5f23880d1764be2616b7084317f
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-07-03 17:34:05 +0100

    expose the chain id

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,4 @@
-export { Transaction } from 'simple-uniswap-sdk';
+export { ChainId, Transaction } from 'simple-uniswap-sdk';
 export * from './models';
 export { UniswapDappSharedLogic } from './uniswap-dapp-shared-logic';
 export { Utils } from './utils';
