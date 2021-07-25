fe7bd6cebb021b0bcc6cee16bb646fe839024c6c
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-07-25 18:07:07 +0100

    fix approval positions

--- vue/package/src/internal-components/approval.vue
+++ vue/package/src/internal-components/approval.vue
@@ -32,32 +32,30 @@ export default {
     v-on:click="logic.approveAllowance()"
   >
     <div class="uni-ic__swap-allow-container">
-      <span>
-        <TokenIcon
-          classes="uni-ic__swap-allow-icon"
-          :context="logic.inputToken.tokenImageContext"
-        />
+      <TokenIcon
+        classes="uni-ic__swap-allow-icon"
+        :context="logic.inputToken.tokenImageContext"
+      />
 
-        <span
-          v-if="
-            miningTransaction === undefined ||
-              miningTransactionStatus === TransactionStatus.rejected
-          "
-          >You must give the Uniswap smart contract permisson to use your
-          {{ tradeContext.fromToken.symbol }}. You only have to do this once per
-          token per uniswap version. Click here to approve the permissons.
-        </span>
-
-        <span
-          v-if="
-            miningTransactionStatus === TransactionStatus.waitingForConfirmation
-          "
-          >Waiting for confirmation....</span
-        >
-        <span v-if="miningTransactionStatus === TransactionStatus.mining"
-          >Waiting for your transaction to be mined...</span
-        >
+      <span
+        v-if="
+          miningTransaction === undefined ||
+            miningTransactionStatus === TransactionStatus.rejected
+        "
+        >You must give the Uniswap smart contract permisson to use your
+        {{ tradeContext.fromToken.symbol }}. You only have to do this once per
+        token per uniswap version. Click here to approve the permissons.
       </span>
+
+      <span
+        v-if="
+          miningTransactionStatus === TransactionStatus.waitingForConfirmation
+        "
+        >Waiting for confirmation....</span
+      >
+      <span v-if="miningTransactionStatus === TransactionStatus.mining"
+        >Waiting for your transaction to be mined...</span
+      >
     </div>
   </button>
 </template>
