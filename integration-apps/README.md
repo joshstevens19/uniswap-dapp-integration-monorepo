6c04655c149d2ab172392f5c71e74f5d0b72295c
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-07-27 10:55:32 +0100

    create a test integration app for angular package

---
+++ integration-apps/README.md
@@ -0,0 +1,1 @@
+This just has angular, vue and react test apps using the published package from npm to make sure it all works.
--- integration-apps/uniswap-angular-test-integration-app/src/app/app.component.ts
+++ integration-apps/uniswap-angular-test-integration-app/src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // tslint:disable-next-line: object-literal-sort-keys
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
@@ -18,7 +19,7 @@ export class AppComponent implements OnInit {
   /**
    * On load
    */
-  async ngOnInit(): Promise<void> {
+  public async ngOnInit(): Promise<void> {
     // MetaMask
     const accounts = await (window as any).ethereum.request({
       method: 'eth_requestAccounts',
@@ -28,11 +29,14 @@ export class AppComponent implements OnInit {
       supportedNetworkTokens: [
         {
           chainId: ChainId.MAINNET,
+          defaultInputValue: '0.000001',
+          // tslint:disable-next-line: object-literal-sort-keys
           defaultInputToken: ETH.MAINNET().contractAddress,
           defaultOutputToken: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f',
           supportedTokens: [
             { contractAddress: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984' },
             { contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7' },
+            { contractAddress: ETH.MAINNET().contractAddress },
             { contractAddress: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9' },
             { contractAddress: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f' },
           ],
@@ -48,6 +52,7 @@ export class AppComponent implements OnInit {
           ],
         },
       ],
+      // tslint:disable-next-line: object-literal-sort-keys
       ethereumAddress: accounts[0],
       ethereumProvider: (window as any).ethereum,
       // theming: {
