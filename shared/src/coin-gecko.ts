bf11732af4a28be98e9be69811b29ad04735bdf3
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-07-28 11:18:58 +0100

    change fetching the fiat price to every 90 seconds instead of 30

--- shared/src/coin-gecko.ts
+++ shared/src/coin-gecko.ts
@@ -8,8 +8,8 @@ export class CoinGecko {
         timestamp: number;
       }
     | undefined = undefined;
-  // 30 seconds cache
-  private _cacheMilliseconds = 30000;
+  // 90 seconds cache
+  private _cacheMilliseconds = 90000;
   constructor() {}
 
   /**
